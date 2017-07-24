d9467e61aaf4d185ac66c63736305970624be743
John Xu <jiang_xu@hotmail.com>
2017-07-24 22:12:30 +1000

    initial commit

---
+++ src/com/jxu/javaproj/Demo2.java
@@ -0,0 +1,11 @@
+package com.jxu.javaproj;
+
+/**
+ * Created by jiang on 24/07/2017.
+ */
+public class Demo2 {
+    public void performUturn(){
+
+        System.out.println("Uturn is not easy");
+    }
+}
--- src/com/jxu/javaproj/Main.java
+++ src/com/jxu/javaproj/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+        Demo2 demo2 = new Demo2();
+        demo2.performUturn();
     }
 }
